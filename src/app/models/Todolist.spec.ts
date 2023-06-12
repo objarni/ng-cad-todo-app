df81fb94b97a7cdae56ce062e059e7fdd9ac7921
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-06-12 11:02:05 +0200

    Add test for remove

--- src/app/models/Todolist.spec.ts
+++ src/app/models/Todolist.spec.ts
@@ -1,11 +1,14 @@
-import {clickAddCalc} from "./Todolist";
+import {clickAddCalc, clickRemoveCalc} from "./Todolist";
 
-describe("Todolist", () => {
-  it("can add items", () => {
+describe("With a Todolist, you can", () => {
+  it("add an items by clicking Add with a text", () => {
     const newItems = clickAddCalc([], "buy milk")
     expect(newItems.todos).toEqual([{ completed: false, content: 'buy milk' }])
   });
+  it("remove an item by clicking Remove", () => {
+    const initialItems = clickAddCalc([], "buy milk")
+    const newItems = clickRemoveCalc(initialItems.todos, 0)
+    expect(newItems.length).toEqual(0);
+  });
 });
-// clicking remove
 // clicking on item
-// adding item
