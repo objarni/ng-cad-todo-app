ae516083c0b915f88ae55a55ca891a9d03106eff
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-06-12 10:57:26 +0200

    Add first test

--- src/app/models/Todolist.spec.ts
+++ src/app/models/Todolist.spec.ts
@@ -1,5 +1,11 @@
+import {clickAddCalc} from "./Todolist";
+
 describe("Todolist", () => {
-  it("should be true", () => {
-    expect(true).toBe(true);
+  it("can add items", () => {
+    const newItems = clickAddCalc([], "buy milk")
+    expect(newItems.todos).toEqual([{ completed: false, content: 'buy milk' }])
   });
 });
+// clicking remove
+// clicking on item
+// adding item
